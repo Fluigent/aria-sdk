ed083b0d46a7be9d1e758e844076c5c1ef72c73e
Mikael Trellet <mikael.trellet@fluigent.com>
2023-09-01 17:15:21 +0200

    Add dll folder and edit deploy script in Python package

--- python/package/deploy.py
+++ python/package/deploy.py
@@ -16,7 +16,7 @@ dll_dst_name= "aria_sdk.dll"
 
 for arch in ["x86", "x64"]:
     os.makedirs(os.path.join("Aria", "SDK", "shared", "windows", arch))
-    shutil.copy(os.path.join("..", "dlls", "win", arch, dll_dst_name), os.path.join("Aria", "SDK", "shared", "windows", arch, dll_dst_name))
+    shutil.copy(os.path.join("..", "dll", "win", arch, dll_dst_name), os.path.join("Aria", "SDK", "shared", "windows", arch, dll_dst_name))
 
 run_setup("setup.py", ["sdist","--format=zip"])
 
